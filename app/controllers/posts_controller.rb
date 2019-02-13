6a1bf9ae98e3c288ebdebdf3003d2a11a8797581
Ayana Bando <ayanaflax@yahoo.com>
2019-02-13 03:00:07 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,9 +19,9 @@ class PostsController < ApplicationController
   def new
     if params[:author_id] && !Author.exists?(params[:author_id])
     redirect_to authors_path, alert: "Author not found."
-  else
-    @post = Post.new(author_id: params[:author_id])
-  end
+    else
+      @post = Post.new(author_id: params[:author_id])
+    end
   end
 
   def create
